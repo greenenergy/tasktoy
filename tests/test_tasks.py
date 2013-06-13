d82c74ea82e9adf17dd5fcfff72f4cf84679ad1c
Colin Fox <greenenergy@gmail.com>
2013-06-13 09:12:41 -0700

    Added loop test.

--- tests/test_tasks.py
+++ tests/test_tasks.py
@@ -14,3 +14,15 @@ class TaskTestCase(unittest.TestCase):
     def test_task_construction(self):
         t = Task("one")
 
+    def test_task_loops(self):
+        a = Task("a")
+        b = Task("b")
+        c = Task("c") 
+        b.add_prereq(a)
+        c.add_prereq(b)
+        try:
+            a.add_prereq(c)
+            self.fail("Shouldn't have allowed a loop")
+        except ValueError, e:
+            pass
+
